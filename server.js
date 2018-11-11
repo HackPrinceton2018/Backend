fbc880e9f5a2d234c2eff7b682bb4fc04d5ad042
Ayushi Shah <ayushishah@nat-oitwireless-inside-vapornet100-a-976.Princeton.EDU>
2018-11-11 08:43:56 -0500

    server update

--- server.js
+++ server.js
@@ -1,21 +1,95 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
-// GraphQL schema
-var schema = buildSchema(`
-    type Query {
-        message: String
-    }
-`);
-// Root resolver
-var root = {
-    message: () => 'Hello World!'
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+var fs = require('fs');
+var firebase = require("firebase");
+
+// Add additional services that you want to use
+require("firebase/auth");
+require("firebase/database");
+
+const lib = require('lib');
+
+
+
+
+var config = {
+    apiKey: "AIzaSyB-rqQ-XYZn8QqBv1P-Rgz1TeE37cjrCQA",
+    authDomain: "myapplication-2da77.firebaseapp.com",
+    databaseURL: "https://myapplication-2da77.firebaseio.com",
+    projectId: "myapplication-2da77",
+    storageBucket: "myapplication-2da77.appspot.com",
+    messagingSenderId: "487937852151"
 };
-// Create an express server and a GraphQL endpoint
-var app = express();
-app.use('/graphql', express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+firebase.initializeApp(config);
+
+// var speechToText = new SpeechToTextV1({
+//   iam_apikey: 'L1GqNKxjzYyR2_6njbhquV9Mrz8EGCqeCglsX0DWo48n',
+//   url: 'https://stream.watsonplatform.net/speech-to-text/api/'
+// });
+
+
+//
+// router.post("/sendAudio", function (req, res, next) {
+// var audio_file = req.body;
+// var params = {
+//   audio: fs.createReadStream('audio_file'),
+//   content_type: 'audio/flac'
+// };
+//   speechToText.recognize(params, function(err, res) {
+//   if (err)
+//     return callback(err);
+//   else{
+//     var x = res.results[0].alternatives[0].transcript;
+//
+//     callback(null, x);
+//     }
+// });
+
+
+module.exports = async () => {
+
+    var promise = new Promise((resolve, reject) => {
+        var userId = "1v20oY1GeDTe6IIZr6OrEMEt8962"
+
+        const payload = {
+            userId: userId,
+            name: "ayushi123"
+        };
+        firebase
+            .database()
+            .ref("users/" + payload.userId)
+            .set(
+                {
+                    username: payload.name,
+                }
+            )
+            .then(() => { resolve() })
+            .catch((error) => console.log(error));
+
+    })
+    let result = await promise
+
+    let promise1 = new Promise((resolve, reject) => {
+        var userId = "1v20oY1GeDTe6IIZr6OrEMEt8962"
+        firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
+            var username = (snapshot.val() && snapshot.val().username)
+            console.log(username);
+            resolve()
+        })
+
+    })
+    let result1 = await promise1
+    return "Hello ${name}, I built this API with Code on Standard Library!"
+}
+
+
+
+
+// })
+
+// router.get("/getAudio", function(req,res,next){
+//
+// })
+//
+// router.get("/getText", function(req,res,next){
+//
+// })
